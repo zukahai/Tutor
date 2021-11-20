26350d11be94ef0ff6b6f8067d93edaf45b4c5f3
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2021-11-20 08:24:09 +0700

    Update Person.java

--- Java13/11_20_2021/Person.java
+++ Java13/11_20_2021/Person.java
@@ -24,23 +24,37 @@ public class Person {
 		return "[name=" + this.name + ", gender=" + this.gender + ", age=" + this.age + "]";
 	}
 	
+	public String getName() {
+		return name;
+	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
+	public String getGender() {
+		return gender;
+	}
+
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
-	
+
+	public int getAge() {
+		return age;
+	}
+
 	public void setAge(int age) {
 		this.age = age;
 	}
-	
+
 	public static void main(String[] args) {
 		Person minh = new Person();
 		minh.setName("Minh");
 		minh.setAge(18);
 		minh.setGender("Female");
+		minh.setGender("Male");
 		System.out.println(minh.toString());
+		System.out.println(minh.getName());
 	}
 }
